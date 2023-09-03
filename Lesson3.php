d39dc96878ee0312aef9a98cc66578763729911a
Phusanisaj <136965179+Phusanisaj@users.noreply.github.com>
2023-09-03 17:20:07 +0700

    done
    End Lesson4

---
+++ Lesson3.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson3</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container p-5 my-5 bg-dark text-white">
+        <h1>Database to Web with PHP : Lesson3</h1>
+        <p>6401102053104 Miss Phusanisa Jamrat</p>
+    </div>
+    <?php
+    require "Connect.PHP";
+    $query = "SELECT * From tb_section where SECT_TELEPHONE LIKE '%12%' ORDER BY SECT_NAME DESC";
+    $statement = $pdo->prepare($query);
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+    <div class="container mt-3">
+        <h2>Section Data</h2>
+        <p>แสดงข้อมูลทั้งหมด โดยเลือกเฉพาะที่ SECT_TELEPHONE มีเลข 12 และเรียงข้อมูลจาก SECT_NAME จาก Z-A</p>
+        <div class="btn-group btn-group-lg">
+            <button type="button" class="btn btn-info"onclick="window.location.href ='index.php'">กลับไปยังหน้าหลัก
+            </button>
+            <button type="button" class="btn btn-info"onclick="window.location.href ='Lesson1.php'">Lesson 1 : แสดงข้อมูลทั้งหมดโดยเรียงจาก SECT_ID จากมากไปหาน้อย
+            </button>
+            <button type="button" class="btn btn-info"onclick="window.location.href ='Lesson3.php'">Lesson 2 : แสดงข้อมูลเฉพาะ SECT_TELEPHONE และ SECT_NAME โดยเรียงจาก SECT_NAME จาก A-Z
+        </div>
+        <table class="table table-hover">
+        <thead>
+                <tr>
+                    <th>SECT_ID</th>
+                    <th>SECT_NAME</th>
+                    <th>SECT_BUILDING_NAME</th>
+                    <th>SECT_TELEPHONE</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($results as $row) : ?>
+                    <tr>
+                        <td><?php echo $row['SECT_ID']; ?></td>
+                        <td><?php echo $row['SECT_NAME']; ?></td>
+                        <td><?php echo $row['SECT_BUILDING_NAME']; ?></td>
+                        <td><?php echo $row['SECT_TELEPHONE']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
---
+++ Lesson4.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database to Web with PHP : Lesson4</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<!--  jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!--  JavaScript -->
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<!--  CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
+
+
+<body>
+    <div class="container p-5 my-5 bg-dark text-white">
+        <h2>Database to Web with PHP : Lesson4</h2>
+        <p>6401102053104 Miss Phusanisa Jamrat</p>
+    </div>
+    <?php
+    require "Connect.PHP";
+    $query = "SELECT * FROM tb_section";
+    $statement = $pdo->prepare($query);
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+    <div class="container mt-3">
+        <h2>Section Data</h2>
+        <p>Section Data details</p>
+        <div class="btn-group btn-group-lg">
+            <button type="button" class="btn btn-info"onclick="window.location.href ='Lesson1.php'">Lesson 1 : แสดงข้อมูลทั้งหมดโดยเรียงจาก SECT_ID จากมากไปหาน้อย
+            </button>
+            <button type="button" class="btn btn-secondary"onclick="window.location.href ='Lesson2.php'">Lesson 2 : แสดงข้อมูลเฉพาะ SECT_TELEPHONE และ SECT_NAME โดยเรียงจาก SECT_NAME จาก A-Z
+            </button>
+            <button type="button" class="btn btn-info"onclick="window.location.href ='Lesson3.php'">Lesson 3 : แสดงข้อมูลทั้งหมด โดยเลือกเฉพาะที่ SECT_TELEPHONE มีเลข 12 และเรียงข้อมูลจาก SECT_NAME จาก Z-A
+            </button>
+        </div>
+        <table class="table table-hover" id ="myTable">
+            <thead>
+                <tr>
+                    <th>SECT_ID</th>
+                    <th>SECT_NAME</th>
+                    <th>SECT_BUILDING_NAME</th>
+                    <th>SECT_TELEPHONE</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($results as $row) : ?>
+                    <tr>
+                        <td><?php echo $row['SECT_ID']; ?></td>
+                        <td><?php echo $row['SECT_NAME']; ?></td>
+                        <td><?php echo $row['SECT_BUILDING_NAME']; ?></td>
+                        <td><?php echo $row['SECT_TELEPHONE']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+</script>
+
+</body>
+
+</html>
